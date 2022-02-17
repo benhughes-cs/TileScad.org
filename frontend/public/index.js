95adc0fc8150aa0c5b1daaafce43032218169b10
lindseyalbin <lkalbin@uark.edu>
2022-02-17 17:34:20 -0600

    Router Correction

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -5,16 +5,16 @@ import HomePage from "../src/pages/home/Home";
 import DesignPage from "../src/pages/design/Design";
 import AboutPage from "../src/pages/about/About";
 import UploadPage from "../src/pages/upload/Upload";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
-    <Switch>
+    <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<AboutPage />} />
       <Route path="/design" element={<DesignPage />} />
       <Route path="/upload" element={<UploadPage />} />
-    </Switch>
+    </Routes>
     <App />
   </Router>,
   document.getElementById("root")
